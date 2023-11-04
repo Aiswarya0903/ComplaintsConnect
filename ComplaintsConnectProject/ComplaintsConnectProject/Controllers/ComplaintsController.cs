b20bc4738142d9c0a11ebacc8cac4c0cc63c19f2
Sarath Chandra Adusumilli <adusumilli1@usf.edu>
2023-11-04 16:35:48 -0400

    Added ComplaintsController

---
+++ ComplaintsConnectProject/ComplaintsConnectProject/Controllers/ComplaintsController.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Complaints.IBusiness;
+using Complaints.Models;
+using Complaints.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Web;
+using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ComplaintsConnect.Controllers
+{
+    public class ComplaintsController : Controller
+    {
+        IComplaintsManager _icomplaintsManager;
+        public ComplaintsController(IComplaintsManager complaintsManager)
+        {
+            _icomplaintsManager = complaintsManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Home page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// AboutUs Page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Compalints Page
+        /// </summary>
+        /// <returns></returns>
+       
+    }
+}
